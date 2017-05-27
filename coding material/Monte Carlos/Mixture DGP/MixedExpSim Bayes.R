d34ef158e9f6a681411c86a47caa98cb708ac953
bomin8319 <bomin8319>
2017-05-27 16:34:39 -0400

    existing bug

--- coding material/Monte Carlos/Mixture DGP/MixedExpSim Bayes.R
+++ coding material/Monte Carlos/Mixture DGP/MixedExpSim Bayes.R
@@ -43,13 +43,13 @@ setwd("/Users/bomin8319/Desktop/BayesOFsurv/coding material/Monte Carlos/Mixture
 ##########################################################################
 
 #set seed
-set.seed(3)   
+set.seed(300)   
 
 #set the number of observations
 n<-100
 
 #set the number of simulations, and create matrices to store the results
-nsims<-1000
+nsims<-2
 
 
 #history matrix for true estimates
