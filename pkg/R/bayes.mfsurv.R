0028b22f9faef2a62fb8e5b422df6f202bdcf79b
Minhyung Joo <minhyung.joo@gmail.com>
2017-08-27 14:01:51 -0400

    deleted DIC

--- pkg/R/bayes.mfsurv.R
+++ pkg/R/bayes.mfsurv.R
@@ -582,3 +582,48 @@ bayes.mfsurv.formula <- function(formula, data=list(), N, burn, thin, w=c(1,1,1)
   } 
 }
 
+
+
+llFun <- function(est,Y,C,X,Z,data) {
+  n=nrow(data)
+  llik <- matrix(0, nrow=n, ncol = 1)
+  gamma<-est[1:ncol(Z)]
+  beta<-est[(ncol(Z)+1):(length(est)-1)]
+  p<-est[length(est)]
+  p<-exp(p)
+  XB<-X%*%beta
+  ZG<-Z%*%gamma
+  phi<-1/(1+exp(-(ZG+1/p)))
+  llik<-C*(log((1-phi)+phi*exp(XB+1/p)*p*((exp(XB+1/p)*Y)^(p-1))*exp(-(exp(XB+1/p)*Y)^p)))+(1-C)*(log(phi)+-(exp(XB+1/p)*Y)^p)
+  llik<- -1*sum(llik)
+  return(llik)
+}
+
+calculateDIC = function(Y,C,X,Z,data, theta, llFun) {
+  #Calculate L
+  theta_post = cbind(theta$gammas, theta$betas, theta$lambda)
+  theta_hat = apply(theta_post, 2, mean)
+  L = llFun(theta_hat,Y,C,X,Z,data)
+  
+  #Calculate P
+  S = nrow(theta_post) #S = number of iterations
+  #Add up the log likelihoods of each iteration
+  llSum = 0
+  for (s in 1:S) {
+    theta_s = theta_post[s,]
+    llSum = llSum + llFun(theta_s,Y,C,X,Z,data)
+  }
+  
+  P = 2 * (L - (1 / S * llSum))
+  
+  #Calculate DIC
+  DIC = -2 * (L - P)
+  
+  #Return the results
+  list(DIC=DIC, P=P, L=L)
+}
+
+
+
+
+
